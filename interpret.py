9b634e4bf0ea1fd8d7299cc9f689877b7369c20e
Martin Tutek <martin.tutek@gmail.com>
2021-12-08 15:04:15 +0100

    Add padding mask to submodel forward

--- interpret.py
+++ interpret.py
@@ -43,6 +43,7 @@ class Interpreter:
                 **self.attribute_kwargs(captum_inputs, mask_features_by_token=self.mask_features_by_token), # General extra arguments
                 **self.attribute_args # To be added in subclass constructor
             )
+        print("[I]", args['inputs'].shape)
 
         # 2. Attribute
         attributions = self.attribute(**args)
