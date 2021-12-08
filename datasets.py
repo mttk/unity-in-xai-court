c6a7f8914f688966d13bba9f9e64e41917ebd3e5
Martin Tutek <martin.tutek@gmail.com>
2021-12-08 15:16:21 +0100

    Add padding mask to submodel forward

--- datasets.py
+++ datasets.py
@@ -25,7 +25,6 @@ def make_iterable(dataset, device, batch_size=32, train=False, indices=None):
       return -len(tokenized)
 
     def cast_to_device(data):
-      print(data)
       return torch.tensor(np.array(data), device=device)
 
     # Selects examples at given indices to support subset iteration.
