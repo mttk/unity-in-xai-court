1c8dcb99a7eb85c75d27e7703167eb86f845c94b
Martin Tutek <martin.tutek@gmail.com>
2021-12-15 16:01:43 +0100

    Make datasets w/o rationales not fail

--- train.py
+++ train.py
@@ -257,8 +257,9 @@ def interpret_evaluate(interpreters, model, data, args, meta, use_rationales=Tru
     # Unpack batch & cast to device
     (x, lengths), y = batch.text, batch.label
     # print(x.shape)
-    rationale = batch.rationale.detach().cpu().numpy() # These are padded to the batch length
-    rationales.extend([r[:l] for r, l in zip(rationale, lengths)])
+    if use_rationales:
+      rationale = batch.rationale.detach().cpu().numpy() # These are padded to the batch length
+      rationales.extend([r[:l] for r, l in zip(rationale, lengths)])
 
     for k, interpreter in interpreters.items():
       # print(lengths.shape)
