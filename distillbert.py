4fb139b4273791ae927320c512514738d4f31a87
Martin Tutek <martin.tutek@gmail.com>
2022-02-22 16:42:42 +0100

    Remove printouts

--- distillbert.py
+++ distillbert.py
@@ -227,7 +227,10 @@ class DistilBertForSequenceClassification(torch.nn.Module, CaptumCompatible):
                 output_dict[analysis_method] = torch.stack(attentions[analysis_method], dim=1)
 
         if self.num_targets == 1:
+            print("[L]", logits.isnan().any())
             class_probabilities = torch.nn.Sigmoid()(logits)
+            print("[C]", class_probabilities.isnan().any())
+            
         else:
             class_probabilities = torch.nn.Softmax(dim=-1)(logits)
         output_dict["class_probabilities"] = class_probabilities
@@ -244,6 +247,7 @@ class DistilBertForSequenceClassification(torch.nn.Module, CaptumCompatible):
 
                 correct_class_pred = p * label + (1. - p) * (1 - label)
                 #print(correct_class_pred, '\n', p, '\n', label)
+                print(correct_class_pred.shape)
                 return correct_class_pred
             else:
                 # Multiclass case
