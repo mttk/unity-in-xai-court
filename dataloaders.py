af3025f03ae7e6a5badb3729cda13626100cd3cb
Martin Tutek <martin.tutek@gmail.com>
2022-05-24 15:45:38 +0200

    Add ids to jwa-sst

--- dataloaders.py
+++ dataloaders.py
@@ -66,10 +66,18 @@ def make_iterable(dataset, device, batch_size=32, train=False, indices=None):
     if indices is not None:
         dataset = dataset[indices]
 
+    #iterator = BucketIterator(
+    #    dataset,
+    #    batch_size=batch_size,
+    #    bucket_sort_key=instance_length,
+    #    shuffle=train,
+    #    matrix_class=cast_to_device,
+    #)
+
     iterator = BucketIterator(
         dataset,
         batch_size=batch_size,
-        bucket_sort_key=instance_length,
+     #   bucket_sort_key=instance_length,
         shuffle=train,
         matrix_class=cast_to_device,
     )
