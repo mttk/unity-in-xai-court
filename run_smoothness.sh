e94a2c51a4c6a105c37e016dfbe38a6665e1dae9
josipjukic <53186339+josipjukic@users.noreply.github.com>
2023-05-24 23:51:59 +0200

    Delete run_smoothness.sh

--- run_smoothness.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python run.py --model-name JWA --gpu $1 --lr $4 --conicity 0 --tying 0 --l2 0 --save-dir $2 --data $3
-python run.py --model-name JWA --gpu $1 --lr $4 --conicity 0 --tying 10 --l2 0 --save-dir $2 --data $3
-python run.py --model-name JWA --gpu $1 --lr $4 --conicity 5 --tying 0 --l2 0 --save-dir $2 --data $3
